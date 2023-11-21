f7cd01a2c529061518fc4f5f454503605a8ab0f0
marco garofalo <mgarofal@login07.leonardo.local>
2023-11-21 16:55:59 +0100

    initial commit

---
+++ convert.sh
@@ -0,0 +1,37 @@
+contrs_in=("A0P5" "P5P5" "VKVK")
+contrs_out=("P5A0" "P5P5" "VKVK")
+TMOS=("OS" "TM")
+eqop=("equal" "opposite")
+
+ens=("B.72.64" "0.00072" "0.018" "0.020" "mix_fixed"   "end_line"
+    "C.06.112" "0.00060" "0.016" "0.018" "mix_fixed"  "end_line"
+    "C.06.80" "0.00060" "0.016" "0.018"   ""          "end_line" 
+    "D.54.96" "0.00054" "0.013" "0.015"   "mix"          "end_line"  )
+#  "B.72.96" "0.00072" "0.018" "0.020"   
+#  "E.44.112" "0.00072"   "0.018"  "0.020"
+prefixs=("mix_fixed"  )
+
+dir="/leonardo_scratch/large/userexternal/mgarofal/gmtemp"
+dirout="out"
+
+tot_ens=`for i in ${ens[@]}; do echo $i ; done | grep end_line  | wc -l`
+cols=$((${#ens[@]}/$tot_ens))
+
+for ((ie=0;ie<${#ens[@]};ie+=$cols ))
+do
+
+for ((ic=0;ic<${#contrs_in[@]};ic++ ))
+do
+
+for ((it = 0; it < ${#eqop[@]}; it++)); do
+
+    #echo a
+    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/data/${ens[ie+4]}_l_l_${TMOS[it]}_${contrs_in[ic]}    ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_${contrs_out[ic]}.txt
+    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/data/${ens[ie+4]}_l_s1_${TMOS[it]}_${contrs_in[ic]}   ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_mu.${ens[ie+2]}_${contrs_out[ic]}.txt
+    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/data/${ens[ie+4]}_l_s2_${TMOS[it]}_${contrs_in[ic]}   ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_mu.${ens[ie+3]}_${contrs_out[ic]}.txt
+    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/data/${ens[ie+4]}_s1_s1_${TMOS[it]}_${contrs_in[ic]}  ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+2]}_${contrs_out[ic]}.txt
+    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/data/${ens[ie+4]}_s2_s2_${TMOS[it]}_${contrs_in[ic]}  ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+3]}_${contrs_out[ic]}.txt
+
+done
+done
+done
---
+++ convert_sanfo_gm2.jl
@@ -0,0 +1,56 @@
+#print(map(x->string(x, x), ARGS))
+using Printf
+
+if length(ARGS)!=2
+    println("usage: julia convert_sanfo_gmw.jl  binfile  gm2file")
+    exit(1)
+end
+if ARGS[1]==ARGS[2]
+    println("input and output must be different")
+    exit(1)
+end
+
+println("reading: ",ARGS[1])
+fs=filesize(ARGS[1])
+println("size: ",fs)
+io = open(ARGS[1], "r");
+
+Nconfs= read(io, Int64)
+Nhits = read(io, Int64)
+T     = read(io, Int64)
+println("Nconfs: ",Nconfs)
+println("Nhits: ",Nhits)
+println("T: ",T)
+
+if (fs-sizeof(Int64)*3-(Nconfs*sizeof(Float64)*(T/2+1)) !=0)
+    println("size and Nconfs do not match")
+    exit(1)
+end
+
+chunk_size=Int(T/2+1)
+#data = zeros(Float64, (Nconfs,chunk_size ))
+# data=Array{Float32}(undef, chunk_size, Nconfs)
+data=Array{Float64}(undef, Nconfs, chunk_size)
+
+for i in 1:Nconfs
+    for t in 1:chunk_size    
+    data[i,t]=read(io, Float64)
+    end
+end
+# wrong order to read it in one go
+# read!(io, data)
+
+close(io)
+
+out = open(ARGS[2], "w");
+for i in 1:Nconfs
+    println(out,"")
+    @printf(out," # %04d_r0\n",i)
+    println(out,"")
+    for t in 1:chunk_size
+        println(out,data[i,t])
+        # println(out,data[i,t])
+    end
+end
+   
+
