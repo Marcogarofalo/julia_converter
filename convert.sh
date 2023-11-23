99aae8129ad18a9d7c4a55919c3cc09f11be73f2
Marcogarofalo <marcogarofalo31@gmail.com>
2023-11-23 16:04:15 +0100

    no VkVk

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
-contrs_in=("A0P5" "P5P5" "VKVK")
-contrs_out=("P5A0" "P5P5" "VKVK")
+contrs_in=("A0P5" "P5P5") # "VKVK")
+contrs_out=("P5A0" "P5P5") # "VKVK")
 TMOS=("OS" "TM")
 eqop=("equal" "opposite")
 
@@ -12,7 +12,7 @@ ens=("B.72.64" "0.00072" "0.018" "0.020" "mix_fixed"   "end_line"
 prefixs=("mix_fixed"  )
 
 dir="/leonardo_scratch/large/userexternal/mgarofal/gmtemp"
-dirout="out"
+dirout=$1
 
 tot_ens=`for i in ${ens[@]}; do echo $i ; done | grep end_line  | wc -l`
 cols=$((${#ens[@]}/$tot_ens))
