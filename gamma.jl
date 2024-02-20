198d534ebe71a1df0281f06e11c04b687732a7d9
Marcogarofalo <marcogarofalo31@gmail.com>
2024-02-20 17:01:26 +0100

    gamma

---
+++ gamma.jl
@@ -0,0 +1,94 @@
+
+const id::Matrix{Complex{Float64}} = [
+	1.0 0.0 0.0 0.0;
+	0.0 1.0 0.0 0.0;
+	0.0 0.0 1.0 0.0;
+	0.0 0.0 0.0 1.0]
+
+const gx::Matrix{Complex{Float64}} = [
+	 0  0 0 im;
+	 0  0 im 0;
+	 0 -im 0 0;
+	-im  0 0 0]
+
+const gy::Matrix{Complex{Float64}} = [
+	0  0  0  1;
+	0  0 -1  0;
+	0 -1  0  0;
+	1  0  0  0]
+
+
+const gz::Matrix{Complex{Float64}} = [
+	 0  0  im  0;
+	 0  0  0  -im;
+	-im 0  0   0;
+	 0  im 0   0]
+
+const gt::Matrix{Complex{Float64}} = [
+	1  0  0  0;
+	0  1  0  0;
+	0  0 -1  0;
+	0  0  0 -1]
+
+const g5::Matrix{Complex{Float64}} = [
+	 0  0 -1  0;
+	 0  0  0 -1;
+	-1  0  0  0;
+	 0 -1  0  0]
+
+const gamma = [id, g5, gx, gy, gz, gt, g5 * gx, g5 * gy, g5 * gz, g5 * gt]
+# this will go in p5,id, p5Vk, VK
+
+struct gamma_struct
+	col::Vector{Int32}
+	val::Vector{ComplexF64}
+end
+
+const G5::gamma_struct=gamma_struct(
+	 [3, 4, 1, 2],
+	 [-1, -1, -1, -1]
+)
+
+const Gt::gamma_struct=gamma_struct(
+	 [1, 2, 3, 4],
+	 [1, 1, -1, -1]
+	)
+
+const Gz::gamma_struct=gamma_struct(
+	 [3, 4, 1, 2],
+	 [im, -im, -im, im]
+	)
+
+const Gy::gamma_struct=gamma_struct(
+	 [4, 3, 2, 1],
+	 [1, -1, -1, 1]
+	)
+const Gx::gamma_struct=gamma_struct(
+	 [4, 3, 2, 1],
+	 [im, im, -im, -im]
+	)
+const Id::gamma_struct=gamma_struct(
+	 [1,2,3,4],
+	 [1, 1,1,1]
+)
+function Base.:*(a::gamma_struct, b::gamma_struct) 
+	Ma=zeros(ComplexF64, 4, 4)
+	Mb=zeros(ComplexF64, 4, 4)
+	for i in 1:4
+		Ma[i,a.col[i]]=a.val[i]
+		Mb[i,b.col[i]]=b.val[i]
+	end
+	Mab=Ma*Mb
+	col=zeros(Int64,4)
+	val=zeros(ComplexF64,4)
+	for i in 1:4
+		for j in 1:4
+			if (abs(Mab[i,j])>1e-7)
+				col[i]=j
+				val[i]=Mab[i,j]
+            end
+		end
+	end
+	return(gamma_struct(col,val))
+end
+const Gamma = [Id, G5, Gx, Gy, Gz, Gt, G5 * Gx, G5 * Gy, G5 * Gz, G5 * Gt]
