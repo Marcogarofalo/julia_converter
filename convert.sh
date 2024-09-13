bbb17a787d9c0b10b8ce0934a238157667053371
Marcogarofalo <marcogarofalo31@gmail.com>
2024-09-13 17:33:45 +0200

    get ll instead of _l_l

--- convert.sh
+++ convert.sh
@@ -3,11 +3,12 @@ contrs_out=("P5A0" "P5P5" "VKVK")
 TMOS=("OS" "TM")
 eqop=("equal" "opposite")
 
-ens=("B.72.64" "0.00072" "0.018" "0.020" "mix_fixed"   "end_line"
+ens=(
+    "B.72.64" "0.00072" "0.018" "0.020" "mix_fixed"   "end_line"
     "C.06.112" "0.00060" "0.016" "0.018" "mix_fixed"  "end_line"
     "C.06.80" "0.00060" "0.016" "0.018"   ""          "end_line" 
     "D.54.96" "0.00054" "0.013" "0.014"   "mix"          "end_line"  
-     "B.72.96" "0.00072" "0.018" "0.019"  ""           "end_line"   
+    "B.72.96" "0.00072" "0.018" "0.019"  ""           "end_line"   
     "E.44.112" "0.00044"   "0.011"  "0.012"  ""           "end_line"      )
 prefixs=("mix_fixed"  )
 
@@ -27,11 +28,16 @@ do
 for ((it = 0; it < ${#eqop[@]}; it++)); do
 
     #echo a
-    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_l_l_${TMOS[it]}_${contrs_in[ic]}    ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_${contrs_out[ic]}.txt
-    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_l_s1_${TMOS[it]}_${contrs_in[ic]}   ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_mu.${ens[ie+2]}_${contrs_out[ic]}.txt
-    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_l_s2_${TMOS[it]}_${contrs_in[ic]}   ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_mu.${ens[ie+3]}_${contrs_out[ic]}.txt
-    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_s1_s1_${TMOS[it]}_${contrs_in[ic]}  ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+2]}_${contrs_out[ic]}.txt
-    julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_s2_s2_${TMOS[it]}_${contrs_in[ic]}  ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+3]}_${contrs_out[ic]}.txt
+    if [ ${ens[ie]} == "E.44.112" ]; then
+        julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_l_l_${TMOS[it]}_${contrs_in[ic]}    ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_${contrs_out[ic]}.txt
+    else
+        julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/ll_${TMOS[it]}_${contrs_in[ic]}    ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_${contrs_out[ic]}.txt
+    fi
+
+    # julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_l_s1_${TMOS[it]}_${contrs_in[ic]}   ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_mu.${ens[ie+2]}_${contrs_out[ic]}.txt
+    # julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_l_s2_${TMOS[it]}_${contrs_in[ic]}   ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+1]}_mu.${ens[ie+3]}_${contrs_out[ic]}.txt
+    # julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_s1_s1_${TMOS[it]}_${contrs_in[ic]}  ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+2]}_${contrs_out[ic]}.txt
+    # julia convert_sanfo_gm2.jl ${dir}/${ens[ie]}/${ens[ie+4]}_s2_s2_${TMOS[it]}_${contrs_in[ic]}  ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie+3]}_${contrs_out[ic]}.txt
 
     
 done
