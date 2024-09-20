7e67e36e9a4cf207a83796905f20fe8000f5262f
Marcogarofalo <marcogarofalo31@gmail.com>
2024-09-20 14:31:43 +0200

    the order of l1 l2

--- convert_charm.sh
+++ convert_charm.sh
@@ -6,7 +6,7 @@ eqop=("equal" "opposite")
 ens=(
     # "C.06.112"  "0.00060"   "0.013"  "0.014"  "0.18000" "0.19000" "0.20000"      "1.6067e-02"  "C112lsc/ave/data"  "end_line"
     # "D.54.96"  "0.00054"   "0.013"  "0.014"  "0.15000" "0.16000" "0.17000"       "1.3557e-02"  "D96lsc/ave/data"  "end_line"
-    # "E.44.112" "0.00044"   "0.011"  "0.012"  "0.13000" "0.14000" "0.15000"       "1.1759e-02"  "E112lsc/ave/data"  "end_line"
+    # "E.44.112" "0.00044"   "4.3100e-4"  "0.012"  "0.13000" "0.14000" "0.15000"       "1.1759e-02"  "E112lsc/ave/data"  "end_line"
 )
 prefixs=("mix_fixed")
 
@@ -31,14 +31,18 @@ for ((ie = 0; ie < ${#ens[@]}; ie += $cols)); do
             julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 8]}/_s_c2_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 7]}_mu.${ens[ie + 5]}_${contrs_out[ic]}.txt
             julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 8]}/_s_c3_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 7]}_mu.${ens[ie + 6]}_${contrs_out[ic]}.txt
 
+            julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 8]}/_l1_s_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 2]}_mu.${ens[ie + 7]}_${contrs_out[ic]}.txt
+            julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 8]}/_l2_s_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 1]}_mu.${ens[ie + 7]}_${contrs_out[ic]}.txt
+
+
         done
     done
 done
 
 ########################### C80
 ens=(
-    # "C.06.80" "0.00060" "0.0005850" "0.014" "0.18000" "0.19000" "there is no 3rd charm" "1.6000e-02" "1.7000e-02" "C80lsc/ave/data" "end_line"
-      "B.72.64" "0.00720" "0.0006675" "0.014" "0.23000" "0.24000" "there is no 3rd charm" "1.7500e-02" "1.8500e-02" "B64lsc/ave/data" "end_line"
+    # "C.06.80" "0.0005850" "0.00060" "0.014" "0.18000" "0.19000" "there is no 3rd charm" "1.6000e-02" "1.7000e-02" "C80lsc/ave/data" "end_line"
+    "B.72.64" "0.0006675" "0.00072" "0.014" "0.23000" "0.24000" "there is no 3rd charm" "1.7500e-02" "1.8500e-02" "B64lsc/ave/data" "end_line"
 
 
 )
@@ -60,6 +64,10 @@ for ((ie = 0; ie < ${#ens[@]}; ie += $cols)); do
             julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 9]}/_c1_c1_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 4]}_${contrs_out[ic]}.txt
             julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 9]}/_c2_c2_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 5]}_${contrs_out[ic]}.txt
             # julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 9]}/_c3_c3_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 6]}_${contrs_out[ic]}.txt
+
+
+            julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 9]}/_s1_s1_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 7]}_${contrs_out[ic]}.txt
+            julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 9]}/_s2_s2_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 8]}_${contrs_out[ic]}.txt
 
             julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 9]}/_s1_c1_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 7]}_mu.${ens[ie + 4]}_${contrs_out[ic]}.txt
             julia convert_sanfo_gm2.jl ${dir}/${ens[ie + 9]}/_s1_c2_${TMOS[$(((it)))]}_${contrs_in[ic]} ${dirout}/c${ens[ie]}_r.${eqop[it]}_mu.${ens[ie + 7]}_mu.${ens[ie + 5]}_${contrs_out[ic]}.txt
