bbea5683b36ce0bf2e38d0548e2d6cf33d61c90c
Marcogarofalo <marcogarofalo31@gmail.com>
2025-04-11 13:09:54 +0200

    fix mass value

--- inputs_rew/input_reweight_light_TM_B64.jl
+++ inputs_rew/input_reweight_light_TM_B64.jl
@@ -4,10 +4,10 @@ const T::Int32 = 1
 const L::Int32 = 64
 const beta = 1.778000000000 ##check
 const kappa = 0.1394265 ##check
-const masses_in = [0.017846601]
-const masses_out = [0.018267]
+const masses_in = [0.00072]
+const masses_out = [0.0006669]
 const reduce_sources_by::Int32 = 1
-const outname::String = "reweight_light_TM_B64.dat"
+const outname::String = "out_rew/reweight_light_TM_B64.dat"
 const pattern_after_rep::String = "_l/"
 const confs::Vector{String} = [
 	"cB211.072.64b_l/reweighting_factor.data.01496",
