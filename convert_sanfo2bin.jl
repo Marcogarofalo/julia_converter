02e5c5f137ab5b8bc3421e97dbf34294a505f391
Marcogarofalo <marcogarofalo31@gmail.com>
2026-02-17 19:25:37 +0100

    convert files in gm2-misstuning

---
+++ convert_sanfo2bin.jl
@@ -0,0 +1,106 @@
+#print(map(x->string(x, x), ARGS))
+using Printf
+
+include("./read_hdf5.jl")
+include("./gamma.jl")
+# include("./read_LIBE_open.jl")
+include("binning.jl")
+
+include("modules_rew.jl")    # Load the file
+
+using .modules_rew
+
+function main()
+	println(length(ARGS))
+	if length(ARGS)!=7
+		println("usage: julia convert_sanfo_gmw.jl  binfile  gm2file conflist   beta kappa  mu1 mu2")
+		exit(1)
+	end
+	if ARGS[1]==ARGS[2]
+		println("input and output must be different")
+		exit(1)
+	end
+
+	println("reading: ", ARGS[1])
+	fs=filesize(ARGS[1])
+	println("size: ", fs)
+	io = open(ARGS[1], "r");
+
+	Nconfs = read(io, Int64)
+	Nhits = read(io, Int64)
+	T::Int32 = read(io, Int64)
+	println("Nconfs: ", Nconfs)
+	println("Nhits: ", Nhits)
+	println("T: ", T)
+
+	if (fs-sizeof(Int64)*3-(Nconfs*sizeof(Float64)*(T/2+1)) != 0)
+		println("size and Nconfs do not match")
+		exit(1)
+	end
+
+	chunk_size=Int(T/2+1)
+	#data = zeros(Float64, (Nconfs,chunk_size ))
+	# data=Array{Float32}(undef, chunk_size, Nconfs)
+	data::Array{Float64} = zeros(Float64, Nconfs, T)
+
+	for i in 1:Nconfs
+		for t in 1:chunk_size
+			data[i, t]=read(io, Float64)
+		end
+	end
+	# wrong order to read it in one go
+	# read!(io, data)
+
+	confs = readlines(ARGS[3])
+	if length(confs) != Nconfs
+		error("File length mismatch: from bin file $Nconfs confs, but conflist has  $(length(confs)) lines.")
+	end
+	L = T/2
+	ncorr::Int32 = 1
+	beta = parse(Float64, ARGS[4])
+	kappa = parse(Float64, ARGS[5])
+	masses::Vector{Float64} = [parse(Float64, ARGS[6]), parse(Float64, ARGS[7])]
+	oranges::Vector{Float64} = []
+	gamma_list::Vector{String} = ["P5P5"]
+	close(io)
+	sizeblock::Int32 = ncorr * 2 * T #  ncorr *reim*T
+	mus::Array{Float64} = masses
+	head::header = header(length(confs), T, L, ncorr, beta, kappa, masses, [0], [0.0], gamma_list, confs, [], oranges, sizeblock)
+
+
+    conf_int::Vector{Int32} = Vector{Int32}(undef, length(confs))
+	
+	for (i, conf) in enumerate(confs)
+        num_str = split(conf, '_')[1] 
+		conf_int[i] = parse(Int32, num_str)
+	end
+	#### write 
+
+	outfile = open(ARGS[2], "w")
+	print(head, outfile)
+	flush(outfile)
+	flush(stdout)
+	for (ic, conf) in enumerate(confs)
+		write(outfile, conf_int[ic])
+		for t in 1:T
+			write(outfile, data[ic, t])
+			write(outfile, Float64(0.0))
+		end
+
+
+	end
+	# out = open(ARGS[2], "w");
+	# for i in 1:Nconfs
+	#     println(out,"")
+	#     @printf(out,"# %04d_r0\n",i)
+	#     println(out,"")
+	#     for t in 1:chunk_size
+	#         println(out,data[i,t])
+	#         # println(out,data[i,t])
+	#     end
+	# end
+
+end
+
+main()
+
---
+++ run_sanfo_gm2.sh
@@ -0,0 +1,65 @@
+
+
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_l1_c1_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_l1_c1.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                      0.00072   0.23
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_l1_c2_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_l1_c2.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                    0.00072   0.24                      
+
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_l2_c1_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_l2_c1.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                      0.0006675   0.23
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_l2_c2_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_l2_c2.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                    0.0006675   0.24   
+
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_s1_c_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_s1_c.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                    0.011759   0.142700769256
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_s2_c_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_s2_c.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                    0.012077   0.142700769256
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_s_c1_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_s_c1.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                    0.011759   0.13
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_s_c2_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_s_c2.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                    0.011759   0.14
+
+julia convert_sanfo2bin.jl /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/mix_s_c3_TM_P5P5\
+                    /home/garofalo/analysis/flow/data_mesons/E112_s_c3.bin\
+                    /home/garofalo/analysis/gm2-mistuning/multi_val_masses_correlators/charm_E_bin/E.44.112/confsList_lsc \
+                    1.96  0.13741288\
+                    0.011759   0.15                                        
+
+
+julia convert_blind_format_2bin.jl /home/garofalo/analysis/gm2-mistuning/P5P5_LMA_confs/D96/P5P5_tm_mu5.4000e-04_LMA.bin\
+                    /home/garofalo/analysis/flow/data/D96_P5P5_l1_l1_LMA.bin\
+                    /home/garofalo/analysis/gm2-mistuning/P5P5_LMA_confs/D96/D96_nconfs_688_neta_960.txt \
+                    1.9  0.137972\
+                    0.00054   0.00054 
